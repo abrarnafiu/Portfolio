a93d9f1d0a6de72c67ff7967f60fc03bdaad998d
Abrar <abrarnafiu@gmail.com>
2025-04-21 05:25:49 -0400

    fixed error

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Flex, Button, HStack, Text, Link } from '@chakra-ui/react'
+import { Box, Flex, Button, HStack, Text} from '@chakra-ui/react'
 import { motion } from 'framer-motion'
 
 const MotionBox = motion(Box)
